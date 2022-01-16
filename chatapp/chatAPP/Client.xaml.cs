f1fa3b7f90473ffa2c40dd0ba663cc90f646bed6
pard1s <76449700+pard1s@users.noreply.github.com>
2022-01-16 20:02:47 +0330

    client

---
+++ chatapp/chatAPP/Client.xaml.cs
@@ -0,0 +1,93 @@
+﻿using SimpleTcp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace chatAPP
+{
+    /// <summary>
+    /// Interaction logic for Client.xaml
+    /// </summary>
+    public partial class Client : Window
+    {
+        string serverIpPort;
+        public Client(string myusername, string ipport)
+        {
+            InitializeComponent();
+            serverIpPort = ipport;
+            txtMyusername.Text = myusername;
+        }
+        SimpleTcpClient client;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            client = new SimpleTcpClient(serverIpPort);
+            client.Events.Connected += Events_Connected;
+            client.Events.Disconnected += Events_Disconnected;
+            client.Events.DataReceived += Events_DataReceived;
+            try
+            {
+                client.Connect();
+                if (client.IsConnected)
+                {
+                    string usrnametoserver = "#l*o*g*g*e*d*i*n*u*s*e*r#" + txtMyusername.Text;
+                    client.Send(usrnametoserver);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Events_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(delegate
+            {
+                txtLog.Text += Encoding.UTF8.GetString(e.Data) + Environment.NewLine ;
+            }));
+            
+        }
+
+        private void Events_Disconnected(object sender, ClientDisconnectedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(delegate
+            {
+                txtLog.Text += $"Server disconnected. {Environment.NewLine}";
+            }));
+            
+        }
+
+        private void Events_Connected(object sender, ClientConnectedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(delegate
+            {
+                txtLog.Text += $"connected to the server. {Environment.NewLine}";
+            }));
+            
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (client.IsConnected)
+            {
+                if (!string.IsNullOrEmpty(txtMessage.Text))
+                {
+                    client.Send(txtDestusername.Text + ":" + txtMessage.Text);
+                    txtLog.Text += $"{txtMyusername.Text}: {txtMessage.Text}{Environment.NewLine}";
+                    txtMessage.Text = string.Empty;
+                }
+                
+            }
+        }
+    }
+}
---
+++ chatapp/chatAPP/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+using System.Security.Cryptography;
+
+namespace chatAPP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            initDB();  //connect to database
+            
+        }
+        // a reference to databse connection
+        private SQLiteConnection dbConnection;
+        private void initDB()
+        {
+            // connect to databse
+            String path = @"data\database.db";
+            dbConnection = new SQLiteConnection("Data Source=" + path + ";Version=3;");
+            dbConnection.Open();
+
+
+        }
+        public string getHash(string pass)
+        {
+            //converts a password to a hash value
+            var data = Encoding.Unicode.GetBytes(pass);
+            var sha1 = new SHA1CryptoServiceProvider();
+            var sha1data = sha1.ComputeHash(data);
+            return Convert.ToBase64String(sha1data);
+        }
+        private void btnLogin_clicked(object sender, RoutedEventArgs e)
+        {
+            if(txtUsername.Text == "" || txtPassword.Text == "" || txtServer.Text == "")
+            {
+                MessageBox.Show("please fill all the fields", "empty field error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            else
+            {
+                string hashedPass = getHash(txtPassword.Text);
+                string sql = "SELECT password AS pass FROM users WHERE username='" + txtUsername.Text + "'";
+                SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+                reader.Read();
+
+                // check if username exists within database
+                if (reader.HasRows)
+                {
+                    //if the entered password is correct
+                    if ((reader["pass"] + "").Equals(getHash(txtPassword.Text)))
+                    {
+                        Client client = new Client(txtUsername.Text, txtServer.Text);
+                        dbConnection.Close();
+                        client.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("wrong password", "incorrect input error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("wrong username", "incorrect input error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+
+            }
+        }
+        private void btnSignup_clicked(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text == "" || txtPassword.Text == "" || txtServer.Text == "")
+            {
+                MessageBox.Show("please fill all the fields", "empty field error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            else
+            {
+                string sql = "INSERT INTO users(username, password) VALUES('"+ txtUsername.Text +"', '" + getHash(txtPassword.Text) +"')";
+                try
+                {
+                    SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+                    SQLiteDataReader reader = command.ExecuteReader();
+                    MessageBox.Show("you have been successfully registered", "signed up", MessageBoxButton.OK, MessageBoxImage.Information);
+                    reader.Close();
+                }
+                catch(Exception exp)
+                {
+                    MessageBox.Show("you may have an empty filed or your username is already taken!", "taken username", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+    }
+}
